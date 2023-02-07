bf1dcd65b9e0d85c6e935f25eebdf699865549bb
wangbei16 <wangbei6@jd.com>
2023-02-07 11:15:16 +0800

    init

---
+++ src/pages/a/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {NoticeBar} from "@test/nutui-react-taro"
+
+export default () => {
+  const uploadUrl = 'https://my-json-server.typicode.com/linrufeng/demo/posts'
+  return <NoticeBar  scrollable text={"asdadsadsads"} />
+};
+
